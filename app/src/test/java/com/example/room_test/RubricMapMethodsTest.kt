0ec4550896fe9950cb776c024805a4d1e4692f5e
Vlad Zhavoronkov <vlad.zhavoronkov@bytepace.com>
2019-10-17 18:51:49 +0600

    Start implementing `RubricMapMethodsTest` like subclass of `GenericMapMethodsTest`

--- app/src/test/java/com/example/room_test/RubricMapMethodsTest.kt
+++ app/src/test/java/com/example/room_test/RubricMapMethodsTest.kt
@@ -1,2 +1,27 @@
 package com.example.room_test
 
+import com.example.room_test.entity_utils.RubricDao
+import com.example.room_test.entity_utils.RubricUtils
+import com.example.room_test.helpers.GenericMapMethodsTest
+import com.example.room_test.helpers.Mapper
+import com.example.room_test.mock_dtos.*
+import java.util.*
+
+private fun newId() = UUID.randomUUID().mostSignificantBits
+fun rubricMock(): Rubric = Rubric(newId(), "Lorem")
+fun gradeMocks(rubricId: Long): List<Grade> = (0..10).map {
+    Grade(newId(), score = it, title = "$it", rubricId = rubricId)
+}
+
+class MockRubricUtils(dao: RubricDao) :
+    RubricUtils<MockGradeDTO, MockRubricDTO>(dao, MockRubricDTO::class.java, MockGradeDTO::class.java),
+    Mapper<Rubric, RubricWithRelations, MockRubricDTO>
+
+class RubricMapMethodsTest :
+    GenericMapMethodsTest<Rubric, RubricWithRelations, MockRubricDTO, MockRubricUtils>()
+{
+    override val newEntityWithRelations: RubricWithRelations
+        get() = RubricWithRelations(
+            rubric = rubricMock(),
+            grades = )
+}
