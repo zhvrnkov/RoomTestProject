7dfdc486bf261748ec5a6a15758d7ebcafa786e4
Vlad Zhavoronkov <vlad.zhavoronkov@bytepace.com>
2019-10-23 14:44:34 +0600

    Add `AppTypeConverter` and tests for it

--- app/src/main/java/com/example/room_test/database.kt
+++ app/src/main/java/com/example/room_test/database.kt
@@ -4,21 +4,63 @@ import androidx.room.*
 import com.example.room_test.entities.*
 import com.example.room_test.entity_utils.*
 
-/*@Database(
+class AppTypeConverter {
+    @TypeConverter
+    fun listOfLongsToString(value: List<Long>): String = value.toString()
+    @TypeConverter
+    fun stringToListOfLongs(value: String): List<Long> = value
+        .substring(1 until (value.length - 1))
+        .split(", ")
+        .mapNotNull { it.toLongOrNull() }
+
+    @TypeConverter
+    fun listOfStringsToString(value: List<String>): String = value.toString()
+    @TypeConverter
+    fun stringToListOfStrings(value: String): List<String> = value
+        .substring(1 until (value.length - 1))
+        .split(", ")
+
+    @TypeConverter
+    fun mapIntIntToString(value: Map<Int, Int>): String = value.toString()
+    @TypeConverter
+    fun stringToMapIntInt(value: String): Map<Int, Int> = value
+        .substring(1 until value.length - 1)
+        .split(", ")
+        .mapNotNull {
+            val t = it.split("=")
+            val fst = t.firstOrNull()?.toIntOrNull()
+            val snd = t.lastOrNull()?.toIntOrNull()
+            val output = if (fst == null || snd == null) {
+                null
+            } else { Pair(fst, snd) }
+
+            output
+        }.toMap()
+}
+
+@Database(
     entities = [
+        School::class,
+        Instructor::class,
         Rubric::class,
         Grade::class,
         SkillSet::class,
         Microtask::class,
         Assessment::class,
         MicrotaskGrade::class,
-        Student::class,
-        School::class,
-        Instructor::class
+        Student::class
     ],
     version = 1
 )
+@TypeConverters(AppTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun schooldDao(): SchoolDao
+    abstract fun instructorDao(): InstructorDao
     abstract fun rubricDao(): RubricDao
     abstract fun gradeDao(): GradeDao
-}*/
+    abstract fun skillSetDao(): SkillSetDao
+    abstract fun microtaskDao(): MicrotaskDao
+    abstract fun assessmentDao(): AssessmentDao
+    abstract fun microtaskGradeDao(): MicrotaskGradeDao
+    abstract fun studentDao(): StudentDao
+}
--- app/src/test/java/com/example/room_test/AppConverterTest.kt
+++ app/src/test/java/com/example/room_test/AppConverterTest.kt
@@ -1,3 +1,32 @@
 package com.example.room_test
 
-class AppConverterTest
+import org.junit.Test
+import org.junit.Assert.*
+import java.util.*
+
+class AppConverterTest {
+    private val converter = AppTypeConverter()
+    private val uuid get() =  UUID.randomUUID()
+
+    @Test
+    fun stringToListOfLongs() {
+        val list = (0..10)
+            .map { uuid.mostSignificantBits }
+        assertEquals(list, converter.stringToListOfLongs(list.toString()))
+    }
+
+    @Test
+    fun stringToListOfStrings() {
+        val list = (0..10)
+            .map { uuid.toString() }
+        assertEquals(list, converter.stringToListOfStrings(list.toString()))
+    }
+
+    @Test
+    fun stringToMapIntInt() {
+        val map = (0..10)
+            .map { Pair(uuid.hashCode(), uuid.hashCode()) }
+            .toMap()
+        assertEquals(map, converter.stringToMapIntInt(map.toString()))
+    }
+}
